bac3856656abe580ec336ec735e8d9fdaee3c1f4
Nirmal <Nirmal@admin.com>
2024-05-02 19:41:56 +0400

    new

--- main.js
+++ main.js
@@ -6,28 +6,29 @@ const app=express();
 app.use(bodyParser.json());  // parse requests of content-type - application/json
 app.use(bodyParser.urlencoded({ extended: true }));  // parse requests of content-type - application/x-www-form-urlencoded
 
-
+// backendurl='http://localhost:3000';
+backendurl='https://diexamplebackend.onrender.com'
 //index page
 app.get('/', (req,res) => {
-    res.render('index.ejs');
+    res.render('index.ejs', {backendurl: backendurl});
 });
 
 
 //login page
 app.get('/login', (req,res) => {
-    res.render('login.ejs');
+    res.render('login.ejs', {backendurl: backendurl});
 });
 
 
 //register page
 app.get('/register', (req,res) => {
-    res.render('register.ejs');
+    res.render('register.ejs', {backendurl: backendurl});
 });
 
 
 //journal individual page
 app.get('/page', (req,res) => {
-    res.render('page.ejs');
+    res.render('page.ejs', {backendurl: backendurl});
 });
 
 
